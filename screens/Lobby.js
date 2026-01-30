b42548452cbe3ae8713cc0992fab34c7749ad1cc
3mgaines6-pixel <3mgaines6@gmail.com>
2026-01-29 22:37:05 -0500

    Add Stretch Studio button to Lobby screen

--- screens/Lobby.js
+++ screens/Lobby.js
@@ -4,6 +4,7 @@ export function Lobby() {
     <h1>Lobby</h1>
     <button onclick="renderScreen('CardioStudio')">Cardio Studio</button>
     <button onclick="renderScreen('StrengthStudio')">Strength Studio</button>
+    <button onclick="renderScreen('StretchStudio')">Stretch Studio</button>
   `;
   return container;
 }
