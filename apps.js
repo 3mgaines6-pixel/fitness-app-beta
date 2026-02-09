daeba8d6731237b003811f729e69984d64cec557
3mgaines6-pixel <3mgaines6@gmail.com>
2026-02-09 17:28:21 -0500

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,14 +1,12 @@
 import { Splash } from "./screens/Splash.js";
 import { GymFloor } from "./screens/GymFloor.js";
-
 import { Machine } from "./screens/Machine.js";
-
 
 import { CardioStudio } from "./screens/CardioStudio.js";
 import { StrengthStudio } from "./screens/StrengthStudio.js";
 import { StretchStudio } from "./screens/StretchStudio.js";
 
-export function renderScreen(screenName) {
+export function renderScreen(screenName, data) {
   const app = document.getElementById("app");
   if (!app) return;
 
@@ -19,10 +17,9 @@ export function renderScreen(screenName) {
       screen = Splash();
       break;
 
-   case "GymFloor":
-  screen = GymFloor();
-  break;
-
+    case "GymFloor":
+      screen = GymFloor();
+      break;
 
     case "CardioStudio":
       screen = CardioStudio();
@@ -35,61 +32,20 @@ export function renderScreen(screenName) {
     case "StretchStudio":
       screen = StretchStudio();
       break;
-case "Machine-15":
-  screen = Machine15();
-  break;
-case "Machine-12":
-  screen = Machine12();
-  break;
-case "Machine-13":
-  screen = Machine13();
-  break;
-case "Machine-14":
-  screen = Machine14();
-  break;
-case "Machine-3":
-  screen = Machine3();
-  break;
-case "Machine-10":
-  screen = Machine10();
-  break;
-case "Machine-11":
-  screen = Machine11();
-  break;
- case "Machine-7":
-  screen = Machine7();
-  break;
-  case "Machine-6":
-  screen = Machine6();
-  break;
-case "Machine-2":
-  screen = Machine2();
-  break;
-case "Machine-9":
-  screen = Machine9();
-  break;
-case "Machine-8":
-  screen = Machine8();
-  break;
 
-case "Machine-5":
-  screen = Machine5();
-  break;
+    case "Machine":
+      screen = Machine(data);
+      break;
 
-case "Machine-4":
-  screen = Machine4();
-  break;
-
-case "Machine-1":
-  screen = Machine1();
-  break;
-
-
-
-   default:
-  screen = GymFloor();
-
-}
+    default:
+      // Unified machine routing: "Machine-15", "Machine-3", etc.
+      if (screenName.startsWith("Machine-")) {
+        const id = Number(screenName.split("-")[1]);
+        screen = Machine(id);
+      } else {
+        screen = GymFloor();
+      }
+  }
 
   app.innerHTML = "";
   app.appendChild(screen);
@@ -100,5 +56,3 @@ window.renderScreen = renderScreen;
 document.addEventListener("DOMContentLoaded", () => {
   renderScreen("Splash");
 });
-
-
