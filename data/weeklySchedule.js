5cd7cd3af3a2d2f2cabb1419f910a17b6b1bb611
3mgaines6-pixel <3mgaines6@gmail.com>
2026-02-11 19:21:25 -0500

    Update weeklySchedule.js

--- data/weeklySchedule.js
+++ data/weeklySchedule.js
@@ -1,7 +1,47 @@
 export const WEEKLY_SCHEDULE = {
-  Monday:    [15, 12, 13, 14, 3],      // Heavy Upper
-  Tuesday:   [7, 8, 5, 6, 2],     // Heavy Lower
-  Wednesday: [15, 7, 5, 14, 3],     // Full Body
-  Thursday:  [15, 12, 13, 14, 4],        // Light Lower
-  Friday:    [5, 6, 9, 2, 1],      // Upper Light Pump
+  Monday: [
+    15,   // Leg Press (Heavy)
+    12,   // Seated Leg Curl
+    10,   // Prone Leg Curl
+    11,   // Leg Extension
+    13,   // Hip Adductor
+    14    // Hip Abductor
+  ],
+
+  Tuesday: [
+    7,    // Chest Press (Heavy)
+    6,    // Shoulder Press (Heavy)
+    2,    // Triceps Press
+    9,    // Pec Fly / Rear Delt
+    8,    // Lat Pulldown
+    5     // Seated Row
+  ],
+
+  Wednesday: [
+    107,  // Chest Press (Light)
+    2,    // Triceps Press
+    9,    // Pec Fly / Rear Delt
+    8,    // Lat Pulldown
+    5,    // Seated Row
+    4,    // Back Extension
+    1,    // Dependent Curl
+    3     // Ab Crunch (Core)
+  ],
+
+  Thursday: [
+    115,  // Leg Press (Light)
+    12,   // Seated Leg Curl
+    10,   // Prone Leg Curl
+    11,   // Leg Extension
+    13,   // Hip Adductor
+    14    // Hip Abductor
+  ],
+
+  Friday: [
+    8,    // Lat Pulldown
+    5,    // Seated Row
+    9,    // Pec Fly / Rear Delt
+    1,    // Dependent Curl
+    3     // Ab Crunch (Core)
+  ]
 };
