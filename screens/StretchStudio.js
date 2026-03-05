7f4731e3e151688bc513e5904a1c4c5f186cc005
3mgaines6-pixel <3mgaines6@gmail.com>
2026-03-05 12:56:16 -0500

    Update StretchStudio.js

--- screens/StretchStudio.js
+++ screens/StretchStudio.js
@@ -2,8 +2,7 @@ export function StretchStudio() {
   const container = document.createElement('div');
   container.innerHTML = `
     <h1>Stretch Studio</h1>
-    <button onclick="renderScreen('Lobby')">Back to Lobby</button>
+    <button onclick="renderScreen('GymFloor')">Back to Gym Floor</button>
   `;
   return container;
 }
-
